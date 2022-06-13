0dc34234ee916d418e13382a342c7d984c111c97
user <user@users-MacBook-Pro.local>
2022-06-13 16:50:33 +0100

    3rd commit

--- travel_tracker/controllers/countries_controller.py
+++ travel_tracker/controllers/countries_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from flask import Blueprint
 from models.countries import Country
 import repositories.countries_repository as countries_repository
@@ -21,3 +21,29 @@ def add_country():
     every_country = countries_repository.select_all()
     return render_template("/countries/new.html", countries=every_country)
 
+@countries_blueprint.route("/countries", methods=["POST"])
+def create_country():
+    country = request.form['country']
+    region = request.form['region']
+    new_country = Country(country, region)
+    countries_repository.save(new_country)
+    return redirect("/countries")
+
+@countries_blueprint.route("/countries/<id>/edit")
+def edit_country(id):
+    country = countries_repository.select(id)
+    return render_template("/countries/edit.html", country=country)  
+    
+
+@countries_blueprint.route("/countries/<id>", methods=["POST"])
+def update_country(id):
+    name = request.form['name']
+    region = request.form['region']
+    edited_country = Country(name, region, id)
+    countries_repository.update(edited_country)
+    return redirect("/countries")
+
+@countries_blueprint.route("/countries/<id>/delete", methods=["POST"])
+def delete_country(id):
+    countries_repository.delete(id)
+    return redirect("/countries")
--- travel_tracker/controllers/destinations_controller.py
+++ travel_tracker/controllers/destinations_controller.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from flask import Blueprint
 from models.destinations import Destination
-import repositories.attractions_repository as attractions_repository
-import repositories.countries_repository as countries_repository
 import repositories.destinations_repository as destination_repository
 
+
 destinations_blueprint = Blueprint("destinations", __name__)
+
+@destinations_blueprint.route("/destinations", methods=["GET"])
+def destinations():
+    destinations = destination_repository.select_all()
+    return render_template("destinations/index.html", destinations=destinations)
+
+@destinations_blueprint.route("/destinations/<id>")
+def showing_destinations(id):
+    destinations_shown = destination_repository.select(id)
+    return render_template("destinations/show.html", destination=destinations_shown)
+
+
