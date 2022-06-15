f96837841971fb4b9674d7b1639451c33ed38d6f
user <user@users-MacBook-Pro.local>
2022-06-15 10:00:21 +0100

    4th commit

--- travel_tracker/controllers/attractions_controller.py
+++ travel_tracker/controllers/attractions_controller.py
@@ -1,8 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.attractions import Attraction
 import repositories.attractions_repository as attractions_repository
-import repositories.countries_repository as countries_repository
-import repositories.destinations_repository as destination_repository
+import repositories.destinations_repository as destinations_repository
+from models.destinations import Destination
+
 
 attractions_blueprint = Blueprint("attractions", __name__)
+
+@attractions_blueprint.route("/attractions", methods=["GET"])
+def attractions():
+    attractions = attractions_repository.select_all()
+    return render_template("attractions/index.html", attractions=attractions)
+
+@attractions_blueprint.route("/attractions/<id>")
+def showing_attractions(id):
+    attractions_shown = attractions_repository.select(id)
+    return render_template("attractions/show.html", attraction=attractions_shown)
+
+@attractions_blueprint.route("/attractions/new")
+def add_attraction():
+    every_destination = destinations_repository.select_all()
+    return render_template("/attractions/new.html", destinations=every_destination)
+
+@attractions_blueprint.route("/attractions/new", methods=["POST"])
+def create_attraction():
+    name = request.form['name']
+    description = request.form['description']
+    destination = request.form['destination']
+    date = request.form['date']
+    visited = request.form['visited']
+    new_destination = Destination.select(destination)
+    new_attraction = Attraction(name, description, new_destination, date, visited)
+    attractions_repository.save(new_attraction)
+    return redirect("/attractions")
+
+@attractions_blueprint.route("/attractions/<id>edit")
+def edit_attraction(id):
+    attraction = attractions_repository.select(id)
+    every_destination = destinations_repository.select_all()
+    return render_template("/attractions/edit.html", attraction=attraction, destination=every_destination)
+
+@attractions_blueprint.route("/attractions", methods=["POST"])
+def update_attraction():
+    name = request.form['name']
+    description = request.form['description']
+    destination = request.form['destination']
+    date = request.form['date']
+    visited = request.form['visited']
+    new_destination = destinations_repository.select(destination)
+    edited_attraction = Attraction(name, description, new_destination, date, visited)
+    attractions_repository.update(edited_attraction)
+    return redirect("/attractions")
+
+@attractions_blueprint.route("/attractions<id>/delete", methods=["POST"])
+def delete_attraction(id):
+    attractions_repository.delete(id)
+    return redirect("/attractions")
+
+
+
--- travel_tracker/controllers/destinations_controller.py
+++ travel_tracker/controllers/destinations_controller.py
@@ -2,7 +2,8 @@ from flask import Flask, redirect, render_template, request
 from flask import Blueprint
 from models.destinations import Destination
 import repositories.destinations_repository as destination_repository
-
+import repositories.countries_repository as countries_repository
+from models.countries import Country
 
 destinations_blueprint = Blueprint("destinations", __name__)
 
@@ -15,5 +16,39 @@ def destinations():
 def showing_destinations(id):
     destinations_shown = destination_repository.select(id)
     return render_template("destinations/show.html", destination=destinations_shown)
+
+@destinations_blueprint.route("/destinations/new")
+def add_destination():
+    every_country = countries_repository.select_all()
+    return render_template("/destinations/new.html", countries=every_country)
+
+@destinations_blueprint.route("/destinations", methods=["POST"])
+def create_destination():
+    name = request.form['name']
+    country = request.form['country']
+    new_country = countries_repository.select(country)
+    new_destination = Destination(name, new_country)
+    destination_repository.save(new_destination)
+    return redirect("/destinations")
+
+@destinations_blueprint.route("/destinations/<id>/edit")
+def edit_destination(id):
+    destination = destination_repository.select(id)
+    every_country = countries_repository.select_all()
+    return render_template("/destinations/edit.html", destination=destination, countries=every_country)
+
+@destinations_blueprint.route("/destinations/<id>", methods=["POST"])
+def update_destination(id):
+    name = request.form['name']
+    country = request.form['country']
+    new_country = countries_repository.select(country)
+    edited_destination = Destination(name, new_country, id)
+    destination_repository.update(edited_destination)
+    return redirect("/destinations")
+
+@destinations_blueprint.route("/destinations/<id>/delete", methods=["POST"])
+def delete_destination(id):
+    destination_repository.delete(id)
+    return redirect("/destinations")
 
 
